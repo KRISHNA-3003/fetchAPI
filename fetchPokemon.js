c0b8fef4034d82491a9fcf680e60f1ff446cbb58
KRISHNA-3003 <81170346+KRISHNA-3003@users.noreply.github.com>
2024-06-29 19:54:45 +0530

    Update fetchPokemon.js

--- fetchPokemon.js
+++ fetchPokemon.js
@@ -1,25 +1,21 @@
 const https = require('https');
 const fs = require('fs');
 
-// Function to fetch details of a specific Pokémon
 function fetchPokemon(pokemon) {
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`;
 
   https.get(url, (res) => {
     let data = '';
 
-    // Check for invalid Pokemon name
     if (res.statusCode === 404) {
       console.error('Invalid Pokémon name. Please provide a valid name.');
       process.exit(1);
     }
 
-    // A chunk of data has been received
     res.on('data', (chunk) => {
       data += chunk;
     });
 
-    // The whole response has been received
     res.on('end', () => {
       try {
         const pokemonDetails = JSON.parse(data);
@@ -33,7 +29,6 @@ function fetchPokemon(pokemon) {
           console.log(`- ${ability.ability.name}`);
         });
 
-        // Download and save the official artwork sprite
         const spriteUrl = pokemonDetails.sprites.other['official-artwork'].front_default;
         downloadSprite(spriteUrl);
       } catch (e) {
@@ -46,7 +41,6 @@ function fetchPokemon(pokemon) {
   });
 }
 
-// Function to download the sprite image
 function downloadSprite(url) {
   const file = fs.createWriteStream('sprite.png');
   https.get(url, (res) => {
@@ -61,7 +55,6 @@ function downloadSprite(url) {
   });
 }
 
-// Ensure one argument is provided
 if (process.argv.length !== 3) {
   console.error('Please provide exactly one argument: the name of the Pokémon.');
   process.exit(1);
